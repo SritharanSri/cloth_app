41e45b40896c00aedae7b6776f945a15843bb93c
SritharanSri <ssrikalai2255@gmail.com>
2024-03-23 11:10:36 +0530

    Profile settings form

---
+++ Clothing_App/View/Profile_settings.swift
@@ -0,0 +1,18 @@
+//
+//  Profile_settings.swift
+//  Clothing_App
+//
+//  Created by NIBMPC04PC03 on 2024-03-23.
+//
+
+import SwiftUI
+
+struct Profile_settings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Profile_settings()
+}
