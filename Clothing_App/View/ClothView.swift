84dd485ecc0fd0e58b9ae758e27e4b1a389b6167
SritharanSri <ssrikalai2255@gmail.com>
2024-03-23 14:54:51 +0530

    Product View

---
+++ Clothing_App/View/ClothView.swift
@@ -0,0 +1,18 @@
+//
+//  ClothView.swift
+//  Clothing_App
+//
+//  Created by NIBM on 2024-03-23.
+//
+
+import SwiftUI
+
+struct ClothView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ClothView()
+}
